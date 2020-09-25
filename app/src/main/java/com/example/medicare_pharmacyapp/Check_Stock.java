bc5eb9af88023df3e95a6e4b441f29deb1270299
Isini-Dananjana <isinidan97@gmail.com>
2020-09-25 22:11:46 +0530

    stock list search implemented

--- app/src/main/java/com/example/medicare_pharmacyapp/Check_Stock.java
+++ app/src/main/java/com/example/medicare_pharmacyapp/Check_Stock.java
@@ -9,10 +9,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.medicare_pharmacyapp.Model.Stock;
@@ -22,8 +25,14 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class Check_Stock extends AppCompatActivity {
@@ -33,6 +42,13 @@ public class Check_Stock extends AppCompatActivity {
     FloatingActionButton check2;
     private Button editBtn,delBtn;
     private String getText;
+    EditText search;
+    ArrayList<Stock> arrayList;
+
+    private Button SearchBtn;
+    private EditText inputText;
+    private RecyclerView searchList;
+    private String SearchInput;
 
     private String productID = "";
     private DatabaseReference productsRef;
@@ -51,7 +67,20 @@ public class Check_Stock extends AppCompatActivity {
         stockList.setLayoutManager(new LinearLayoutManager(this));
         check2 = findViewById(R.id.fab);
         editBtn = (Button) findViewById(R.id.edit_stock_btn);
+        arrayList = new ArrayList<>();
 
+        inputText = findViewById(R.id.search_product);
+        SearchBtn = findViewById(R.id.search_btn);
+        searchList=findViewById(R.id.search_list);
+
+        SearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SearchInput = inputText.getText().toString();
+                onStartM();
+            }
+        });
 
 
 
@@ -71,6 +100,108 @@ public class Check_Stock extends AppCompatActivity {
 
     }
 
+    private void onStartM() {
+
+        FirebaseRecyclerOptions<Stock> options=new FirebaseRecyclerOptions.Builder<Stock>()
+                .setQuery(ordersRef.orderByChild("pname").startAt(SearchInput), Stock.class)
+                .build();
+
+
+        FirebaseRecyclerAdapter<Stock, StockViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Stock, StockViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull StockViewHolder stockViewHolder, final int option, @NonNull Stock stock) {
+
+                        stockViewHolder.txtProductName.setText("Name: " + stock.getPname());
+                        stockViewHolder.txtProductPrice.setText("price of one product: " + stock.getPrice() + "LKR");
+                        stockViewHolder.txtProductQuantity.setText("Quantity: " + stock.getQuantity());
+                        stockViewHolder.date.setText("Stored date , time : " +stock.getDate()+","+stock.getTime());
+                        stockViewHolder.edit.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                String pID = getRef(option).getKey();
+
+                                Intent intent = new Intent(Check_Stock.this, editStock.class);
+                                intent.putExtra("pid", pID);
+                                startActivity(intent);
+
+                            }
+                        });
+
+
+                        stockViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                CharSequence options[] = new CharSequence[]{
+
+                                        "Yes",
+                                        "No"
+
+
+                                };
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Check_Stock.this);
+                                builder.setTitle("Do you want to delete this  ?");
+
+
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        if(i == 0){
+
+                                            String pID = getRef(option).getKey();
+                                            RemoveOrderc(pID);
+
+                                        }else {
+                                            finish();
+                                        }
+
+                                    }
+                                });
+
+                                builder.show();
+
+                            }
+                        });
+
+                    }
+
+
+
+
+
+                    @NonNull
+                    @Override
+                    public StockViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stock_layout, parent, false);
+                        StockViewHolder holder = new StockViewHolder(view);
+
+                        return holder;
+
+                    }
+                };
+
+
+        stockList.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    private void RemoveOrderc(String pID) {
+
+
+        ordersRef.child(pID).removeValue();
+
+    }
+
+
+
+
+
+
 
 
     @Override
@@ -81,6 +212,7 @@ public class Check_Stock extends AppCompatActivity {
                 new FirebaseRecyclerOptions.Builder<Stock>()
                         .setQuery(ordersRef, Stock.class)
                         .build();
+
 
         FirebaseRecyclerAdapter<Stock, StockViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Stock, StockViewHolder>(options) {
